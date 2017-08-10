4a93225ea91dcaa7b4eef427539f090f64d98122
kcod <ganadara135@gmail.com>
2017-08-10 18:02:06 +0900

    multichain with promise

--- router/main.js
+++ router/main.js
@@ -1,6 +1,6 @@
-//chain sdk parts
-//const chain = require('chain-sdk')
-var bodyParser = require('body-parser');
+"use strict";
+
+
 const connection = {
       port: 7206,     //rpc port
       host: '220.230.112.30',
@@ -29,6 +29,7 @@ app.post('/createAddressPrivateKey/:IDname',function(req,res){
       var sess = req.session;
       var IDofUser = req.params.IDname;
       var result = {};
+//      let addressMy, pubkeyMy, privkeyMy;
 
       // CHECK REQ VALIDITY
       if(!req.body.password || !req.body.IDname){
@@ -53,29 +54,120 @@ app.post('/createAddressPrivateKey/:IDname',function(req,res){
           users[IDofUser] =  req.body;
 
           // SAVE DATA
-          fs.writeFile(__dirname + "/../data/user.json",
-                       JSON.stringify(users, null, '\t'), "utf8", function(err, data){
-      //             result = {"success": 1};
-      //             res.json(result);
-/*              res.render('choice', {
-                  title: "MY HOMEPAGE",
-                  length: 5,
-                  IDname: req.body.IDname
-              })
+          fs.writeFile(__dirname + "/../data/user.json", JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+            if(err){
+                throw err;
+            }
+
+            console.log("call createkeypairs()");
+//        return multichain.validateAddressPromise({address: this.address1})
+            multichain.createkeypairsPromise().then(addrPubPri => {
+                assert(addrPubPri);
+                console.log(addrPubPri);
+/*
+                this.addressMy = addrPubPri[0]["address"];
+                this.pubkeyMy = addrPubPri[0]["pubkey"];
+                this.privkeyMy = addrPubPri[0]["privkey"];
+
 */
+                result["address"] = addrPubPri[0]["address"];
+                result["pubkey"] = addrPubPri[0]["pubkey"];
+                result["privkey"] = addrPubPri[0]["privkey"];
+
+//                console.log("addrPubPri.address : " + addrPubPri.address);
+                console.log("addrPubPri[address] : " + addrPubPri[0]["address"]);
+                console.log("this.addressMy : " + result["address"]);
+//                res.json(result);
+//                importAddress: ["address", {"label": ""}, {"rescan": true}],
+                return multichain.importAddressPromise({
+                  address: result["address"],
+                  rescan: false
+                })
+            })
+            .then(() => {
+    //            assert(privateKey)
+
+                console.log("TEST: GRANT")
+                return multichain.grantPromise({
+                    addresses: result["address"],
+                    permissions: "send,receive,issue,admin"
+                })
+            })
+            .then(() => {
+                console.log("TEST: ISSUE")
+                return multichain.issuePromise({
+                    address: result["address"],
+                    asset: {
+                        name: "bookingcoin",
+                        open: true
+                    },
+                    qty: 1000,      // 기본  100 개  예약할 수 있는 조건 부여
+                    units: 1
+                })
+            })
+            .catch(err => {
+                console.log(err)
+                throw err;
+            })
+
+          })   // fs.writeFile
+      })  // fs.readFile
+
+
+
+
+/*
+
+      multichain.getNewAddressPromise()
+      .then(address => {
+          assert(address, "Could not get new address")
+          this.address1 = address;
+
+          console.log("TEST: VALIDATE ADDRESS")
+          return multichain.validateAddressPromise({address: this.address1})
+      })
+      .then(addrInfo => {
+          assert(addrInfo);
+          assert(addrInfo.isvalid === true);
+          assert(addrInfo.address === this.address1);
+
+          console.log("TEST: DUMP PRIVATE KEY")
+          return multichain.dumpPrivKeyPromise({address: this.address1})
+      })
+      .then(privateKey => {
+          assert(privateKey)
+
+          console.log("TEST: GRANT")
+          return multichain.grantPromise({
+              addresses: this.address1,
+              permissions: "send,receive,issue,admin"
           })
       })
+      .then(permissionsTxid => {
+          assert(permissionsTxid)
 
-      console.log("Running Tests")
-
-
-      multichain.getInfo((err, info) => {
-          if(err){
-            console.log(err);
-              throw err;
-          }
-          console.log(info);
+          console.log("TEST: GET NEW ADDRESS")
+          return multichain.getNewAddressPromise();
       })
+      .catch(err => {
+          console.log(err)
+          throw err;
+      })
+  // grant
+ grant Myaddress receive,send
+
+// issue
+      issue Myaddress asset0 50000 0.001
+      issuefrom pppoooo pppoooo '{"name":"GBP","open":true}' 50000 0.01 0 '{"origin":"uk", "stage":"01", "purpose":"parts prepayment"}'
+      Metadata-only issuance
+      Finally let’s see an example of a reissuance whose sole purpose is to add some metadata to the asset, and not create any new units. This is achieved simply by passing zero for the quantity parameter:
+
+      issuemorefrom pppoooo pppoooo GBP 0 0 '{"approval":"head office"}'
+
+      The metadata-only issuance event can be viewed here:
+
+      listassets GBP true
+*/
 
   });
 
